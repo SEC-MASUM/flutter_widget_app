72a740d712513ed031906769c6b9c6d05fc6e3e7
SEC-MASUM <masum.jess42@gmail.com>
2024-08-10 00:18:04 +0600

    AppBar

--- lib/app_bar/app_bar.dart
+++ lib/app_bar/app_bar.dart
@@ -1,25 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeActivity(),);
-  }
-}
-
-class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({super.key});
 
   MySnackbar(message, context){
     return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
+        SnackBar(content: Text(message))
     );
   }
 
@@ -44,5 +30,4 @@ class HomeActivity extends StatelessWidget{
 
     );
   }
-
 }
