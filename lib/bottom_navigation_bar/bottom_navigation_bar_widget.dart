6c4df26411b4d8ea61e2e9b8a6b1d097afe5efa8
SEC-MASUM <masum.jess42@gmail.com>
2024-08-10 14:38:53 +0600

    BottomNavigationBar

--- lib/bottom_navigation_bar/bottom_navigation_bar_widget.dart
+++ lib/bottom_navigation_bar/bottom_navigation_bar_widget.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeActivity(),
-    );
-  }
-}
-
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class BottomNavigationBarWidget extends StatelessWidget {
+  const BottomNavigationBarWidget({super.key});
 
   MySnackbar(message, context) {
     return ScaffoldMessenger.of(context).showSnackBar(
