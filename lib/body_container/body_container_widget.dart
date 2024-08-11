c4e837432cf0194d743ec59819136a4288ad82b4
SEC-MASUM <masum.jess42@gmail.com>
2024-08-11 17:56:15 +0600

    Body Container

--- lib/body_container/body_container_widget.dart
+++ lib/body_container/body_container_widget.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeActivity(),
-    );
-  }
-}
-
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
-
-  MySnackbar(message, context) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-      ),
-    );
-  }
+class BodyContainerWidget extends StatelessWidget {
+  const BodyContainerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +16,8 @@ class HomeActivity extends StatelessWidget {
         margin: EdgeInsets.all(30),
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
-          color: Colors.yellow,
-          border: Border.all(color: Colors.blue, width: 6)
+            color: Colors.yellow,
+            border: Border.all(color: Colors.blue, width: 6)
         ),
         child: Image.network("https://cdn.iconscout.com/icon/free/png-512/free-flutter-2038877-1720090.png", width: 200, height: 200,),
       ),
