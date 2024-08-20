9ba237564eb9db91aab2476155d21b6207d547da
SEC-MASUM <masum.jess42@gmail.com>
2024-08-20 11:55:27 +0600

    Circular Progress Indicator and Linear Progress Indicator

--- lib/circular_linear_progress_indicator/circular_linear_progress_indicator.dart
+++ lib/circular_linear_progress_indicator/circular_linear_progress_indicator.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeActivity(),
-    );
-  }
-}
-
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class CircularLinearProgressIndicator extends StatelessWidget {
+  const CircularLinearProgressIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
