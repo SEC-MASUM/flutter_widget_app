0d300f4e2497009d17a5f0522e314e7cbcd25caa
SEC-MASUM <masum.jess42@gmail.com>
2024-09-09 10:39:00 +0600

    FractionallySizeBox Widget

--- lib/fractionally_size_box/fractionally_size_box_widget.dart
+++ lib/fractionally_size_box/fractionally_size_box_widget.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeActivity(),
-    );
-  }
-}
-
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class FractionallySizeBoxWidget extends StatelessWidget {
+  const FractionallySizeBoxWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
